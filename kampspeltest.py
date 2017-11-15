395439f998241669a9229fafa4f94bc34fcbd950
WoroSchool <33526356+WoroSchool@users.noreply.github.com>
2017-11-15 23:03:57 +0200

    Skickas upp till molnet

--- kampspeltest.py
+++ kampspeltest.py
@@ -4,7 +4,7 @@
 #GUI
 from tkinter import * 
 
-#För att få olika skada
+#För att få olika attacker
 import random 
 
 def huvudmeny():
@@ -12,10 +12,25 @@ def huvudmeny():
 
 	print('Välkommen till spelet.\nDet är ett rondbaserat kampspel där du strider mot en motståndare.')
 	print('Vad är ditt namn?')
+	return
 
-def introduktion():
-	info=Label(fonster, text='Välkommen till spelet '+namn+'.\nDet är ett rondbaserat kampspel där du strider mot en motståndare.\nDitt mål är att vinna motståndaren.')
-	info.pack()
+#Rensar skärmen och ändrar storleken och mera
+
+def rensa():
+	startknapp.pack_forget()
+	info.pack_forget()
+	fonster.resizable(width=False, height=False)
+	fonster.geometry('1000x700')
+	fonster.title('Kampspel')
+	#Animerad bakgrund
+	canvas = Canvas(fonster)
+	canvas.grid(row = 0, column = 0)
+	photo = PhotoImage(file = 'bakgrund.gif') 
+	canvas.pack(expand = YES, fill = BOTH)
+	#Ena hörnet av bilden i nordväst
+	canvas.create_image(0, 0, image=photo, anchor=NW)
+	fonster.mainloop()
+	return
 
 huvudmeny()
 namn = input()
@@ -29,10 +44,16 @@ else:
 
 if running == True:
 	fonster=Tk()
-	fonster.title('Kampspel')
+	fonster.title('Information')
 	fonster.resizable(width=False, height=False)
-	fonster.geometry('1000x500')
-	introduktion()
+	fonster.geometry('400x100')
+
+	info=Label(fonster, text='Välkommen till spelet '+namn+'.\nDet är ett rondbaserat kampspel där du strider mot en motståndare.\nDitt mål är att vinna motståndaren.')
+	info.pack()
+
+	startknapp=Button(fonster, text='START!',command=lambda:rensa())
+	startknapp.pack()
+
 	fonster.mainloop()
 
 
