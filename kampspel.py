46929288836eb99a9a93bf3b28cf1d5324e36002
WoroSchool <33526356+WoroSchool@users.noreply.github.com>
2017-11-27 21:50:01 +0200

    Nya saker
    Lagat o. förbättrat spelet

--- kampspel.py
+++ kampspel.py
@@ -4,40 +4,58 @@
 #GUI
 from tkinter import * 
 
-#För att få olika skada
+#För att få olika attacker
 import random 
 
 def huvudmeny():
 	#Hur menyn som spelaren väljer med ska se ut
 
 	print('Välkommen till spelet.\nDet är ett rondbaserat kampspel där du strider mot en motståndare.')
+	print('Vad är ditt namn?')
+	return
 
-def namn():
-	a = int(Entry.get(x))
-	namn.configure(text='Ditt namn är: ' +str(a))
+#Rensar skärmen och ändrar storleken och mera
+
+def rensa():
+	startknapp.pack_forget()
+	info.pack_forget()
+	fonster.resizable(width=False, height=False)
+	fonster.geometry('1000x700')
+	fonster.title('Kampspel')
+	#Animerad bakgrund
+	canvas = Canvas(fonster)
+	canvas.grid(row = 0, column = 0)
+	photo = PhotoImage(file = 'bakgrund.gif') 
+	canvas.pack(expand = YES, fill = BOTH)
+	#Ena hörnet av bilden i nordväst
+	canvas.create_image(0, 0, image=photo, anchor=NW)
+	fonster.mainloop()
+	return
 
 huvudmeny()
+namn = input()
+print('Hej '+namn+' vill du starta spelet?')
+starta = input('JA/NEJ\n')
+if starta.upper() == 'JA':
+	running = True
+else:
+	running = False
+	input('Tryck på ENTER för att avsluta...')
 
-fonster=Tk()
-fonster.title('Kampspel')
-fonster.resizable(width=False, height=False)
-fonster.geometry('1000x500')
+if running == True:
+	fonster=Tk()
+	fonster.title('Information')
+	fonster.resizable(width=False, height=False)
+	fonster.geometry('400x100')
 
-#Introduktion
+	info=Label(fonster, text='Välkommen till spelet '+namn+'.\nDet är ett rondbaserat kampspel där du strider mot en motståndare.\nDitt mål är att vinna motståndaren.')
+	info.pack()
 
-info=Label(fonster, text='Välkommen till spelet.\nDet är ett rondbaserat kampspel där du strider mot en motståndare.\nDitt mål är att vinna motståndaren.')
-info.pack()
+	startknapp=Button(fonster, text='START!',command=lambda:rensa())
+	startknapp.pack()
 
-namn=Label(fonster, text='Vad heter du?\n')
-namn.pack()
+	fonster.mainloop()
 
-#Vad vill du heta?
 
-x=Entry(fonster, width=12)
-x.pack()
 
-knapp=Button(fonster,text='Klart',command=lambda:namn())
-knapp.pack()
-
-fonster.mainloop()
-
+	
--- kampspeltest.py
+++ kampspeltest.py
@@ -7,58 +7,102 @@ from tkinter import *
 #För att få olika attacker
 import random 
 
-def huvudmeny():
-	#Hur menyn som spelaren väljer med ska se ut
+#Bakgrundsmusik
+import winsound
 
-	print('Välkommen till spelet.\nDet är ett rondbaserat kampspel där du strider mot en motståndare.')
-	print('Vad är ditt namn?')
-	return
+def slag():
+	global troll_hp
+	troll_hp = troll_hp - random.randint(18, 25)
+	return troll_hp
+
+def superslag():
+	global troll_hp
+	troll_hp = troll_hp - random.randint(10, 35)
+	return troll_hp
+
+def hela():
+	global spelare_hp
+	spelare_hp = spelare_hp + random.randint(20, 25)
+	return spelare_hp
+
+def klar_namn():
+	namn.grid_forget()
+	inloggning.grid_forget()
+	klar.grid_forget()
+	start=Label(frame, text='Hej '+namn.get()+' vill du starta spelet?\nDu måste slå ner trollet för att vinna spelet!\nDu har tre attacker att välja på.\nVarje spelare har 100hp och vinnaren är den som får ner den andra till 0.')
+	start.grid(padx=20,pady=20)
+	startknapp.grid(padx=10,pady=10)
 
 #Rensar skärmen och ändrar storleken och mera
-
 def rensa():
-	startknapp.pack_forget()
-	info.pack_forget()
-	fonster.resizable(width=False, height=False)
-	fonster.geometry('1000x700')
-	fonster.title('Kampspel')
+	startknapp.grid_forget()
+	root.resizable(width=False, height=False)
+	root.geometry('1000x650')
+	root.title('Kampspel')
 	#Animerad bakgrund
-	canvas = Canvas(fonster)
-	canvas.grid(row = 0, column = 0)
-	photo = PhotoImage(file = 'bakgrund.gif') 
-	canvas.pack(expand = YES, fill = BOTH)
-	#Ena hörnet av bilden i nordväst
-	canvas.create_image(0, 0, image=photo, anchor=NW)
-	fonster.mainloop()
-	return
+	#Fick hitta ett sätt att undvika .grid och .pack eftersom de inte går ihop
+	bg_bild = PhotoImage(file = 'bakgrund.gif') 
+	x = Label(image=bg_bild)
+	x.grid(row=0, column=0)
 
-huvudmeny()
-namn = input()
-print('Hej '+namn+' vill du starta spelet?')
-starta = input('JA/NEJ\n')
-if starta.upper() == 'JA':
-	running = True
-else:
-	running = False
-	input('Tryck på ENTER för att avsluta...')
+	atk1=Button(toolframe, text='Slag',padx=20,pady=10,background='gray',command=lambda:slag())
+	atk1.grid()
 
-if running == True:
-	fonster=Tk()
-	fonster.title('Information')
-	fonster.resizable(width=False, height=False)
-	fonster.geometry('400x100')
+	atk2=Button(toolframe, text='Superslag',padx=20,pady=10,background='gray',command=lambda:superslag())
+	atk2.grid()
 
-	info=Label(fonster, text='Välkommen till spelet '+namn+'.\nDet är ett rondbaserat kampspel där du strider mot en motståndare.\nDitt mål är att vinna motståndaren.')
-	info.pack()
+	atk3=Button(toolframe, text='Hela',padx=20,pady=10,background='gray',command=lambda:hela())
+	atk3.grid()
 
-	startknapp=Button(fonster, text='START!',command=lambda:rensa())
-	startknapp.pack()
+	hp=Label(toolframe, text='Ditt liv:'+str(spelare_hp)+'')
+	hp.grid(row=0, column=1)
 
-	fonster.mainloop()
+	trollhp=Label(toolframe, text='Trollets liv:'+str(troll_hp)+'')
+	trollhp.grid(row=1, column=1)
+
+	root.mainloop()
+	#Gav upp med att försöka lägga in riddare och troll som skilt
+	#Lade till dem till bilden istället.
+
+spelare_hp = 100
+troll_hp = 100
+
+if spelare_hp > 100:
+	spelare_hp = 100
+
+if troll_hp > 100:
+	troll_hp = 100
+
+root=Tk()
+root.title('Information')
+root.resizable(width=False, height=False)
+
+frame = Frame(root)
+frame.grid()
+
+toolframe = Frame(root)
+toolframe.grid()
+		
+inloggning=Label(frame, text='Vad heter du?')
+inloggning.grid()
+namn=Entry(frame)
+namn.grid(padx=100, pady=5)
+klar=Button(frame, text='Klar',command=lambda:klar_namn())
+klar.grid()
+
+startknapp=Button(frame,text='START!',command=lambda:rensa())
+startknapp.grid()
+startknapp.grid_forget()
 
 
 
-	
+
+
+
+
+
+
+          
 
 
 
